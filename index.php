00634067eaa93927ba1e263890f96a3dddff3afe
FedorRomanovski <digitallife2005@rambler.ru>
2021-11-03 23:15:33 +0300

    Изменения в index.php

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
+  <title>База Данных "Метролог"</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  </head>
 <body>
-<<h1>Метролог</h1>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-2">Метролог</span>
+      </a>
 
+      <ul class="nav nav-pills">
+        <!-- <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li> -->
+        <li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">База Данных</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Цены</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
+      </ul>
+    </header>
+  </div>
 </body>
 </html>
