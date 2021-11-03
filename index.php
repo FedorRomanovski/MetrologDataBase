5fc1b8b8e363af111c98958398607b429b711b6a
FedorRomanovski <digitallife2005@rambler.ru>
2021-11-03 23:38:50 +0300

    Добавил футер. Изучил какой класс нужен, чтобы прижать его вниз

--- index.php
+++ index.php
@@ -9,22 +9,38 @@
   <link rel="stylesheet" href="css/style.css">
   </head>
 <body>
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-2">Метролог</span>
-      </a>
 
-      <ul class="nav nav-pills">
-        <!-- <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li> -->
-        <li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">База Данных</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Цены</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
-      </ul>
-    </header>
+    <div class="container">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32">M<use xlink:href="#bootstrap"></use></svg>
+          <span class="fs-2">Метролог</span>
+        </a>
+
+        <ul class="nav nav-pills">
+          <!-- <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li> -->
+          <li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">База Данных</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Цены</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
+        </ul>
+      </header>
+
+        <!--Код для футера-->
+    <div class="container">
+      <footer  class="fixed-bottom">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+        <p class="text-center text-muted">© 2021 Company, Inc</p>
+      </footer>
+    </div>
+
   </div>
 </body>
 </html>
