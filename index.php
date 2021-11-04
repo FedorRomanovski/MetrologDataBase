ab3e073f498353e79be8390b9010766daa5f92f6
FedorRomanovski <digitallife2005@rambler.ru>
2021-11-04 04:39:00 +0300

    Изменил index.php. Добавил колонки для статей

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
 
     <div class="container">
-      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
           <svg class="bi me-2" width="40" height="32">M<use xlink:href="#bootstrap"></use></svg>
           <span class="fs-2">Метролог</span>
@@ -21,21 +21,58 @@
           <!-- <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li> -->
           <li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
           <li class="nav-item"><a href="#" class="nav-link">База Данных</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Цены</a></li>
           <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
           <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
         </ul>
       </header>
+    </div>
+
+
+      <!-- Контейнер для ссылок на статьи -->
+    <div class="container">
+              <div class="row align-items-center">
+                <div class="col">
+                      <div class="card" style="width: 18rem;">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnfq-WbYTqJQE-Ul-c-DYggzSnH0EbaYMUxA&usqp=CAU" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Подробнее</a>
+                      </div>
+                    </div>
+                </div>
+              <div class="col">
+                <div class="card" style="width: 18rem;">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnfq-WbYTqJQE-Ul-c-DYggzSnH0EbaYMUxA&usqp=CAU" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Подробнее</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col">
+                  One of three columns
+            </div>
+            <div class="col">
+                One of three columns
+          </div>
+          <div class="col">
+              One of three columns
+        </div>
+          </div>
+    </div>
+
 
         <!--Код для футера-->
     <div class="container">
-      <footer  class="fixed-bottom">
+      <!-- fixed-bottom -->
+      <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Главная</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">База Данных</a></li>
           <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">О нас</a></li>
         </ul>
         <p class="text-center text-muted">© 2021 Company, Inc</p>
       </footer>
